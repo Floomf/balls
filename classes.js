b6bef3bff37c974d079afd580209e9a49c14aaae
Poffers <mrfrozenfirefly@gmail.com>
2022-08-09 13:06:57 -0700

    Initial commit

---
+++ classes.js
@@ -0,0 +1,100 @@
+class Vector {
+    x;
+    y;
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    add(other) {
+        return new Vector(this.x + other.x, this.y + other.y);
+    }
+
+    sub(other) {
+        return new Vector(this.x - other.x, this.y - other.y);
+    }
+
+    mult(scalar) {
+        return new Vector(this.x * scalar, this.y * scalar);
+    }
+
+    div(scalar) {
+        return new Vector(this.x / scalar, this.y / scalar);
+    }
+
+    dot(other) {
+        return this.x * other.x + this.y * other.y;
+    }
+
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+}
+
+class Ball {
+    position;
+    velocity;
+    radius;
+    colliding = false;
+    active = false;
+
+    constructor(position, velocity, radius) {
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+    }
+
+    move() {
+        this.position = this.position.add(this.velocity);
+    }
+
+    distance(other) {
+        return Math.sqrt((this.position.x - other.position.x) ** 2 + (this.position.y - other.position.y) ** 2);
+    }
+
+    hasCollided(other) {
+        return this.active && other.active && this.distance(other) < this.radius + other.radius;
+    }
+
+    correctOverlap(other) {
+        const distanceBetween = this.distance(other)
+
+        const overlap = this.radius + other.radius - distanceBetween;
+        const xOverlap = (overlap / distanceBetween) * Math.abs(this.position.x - other.position.x);
+        const yOverlap = (overlap / distanceBetween) * Math.abs(this.position.y - other.position.y);
+
+        if (this.position.x > other.position.x) {
+            this.position = this.position.add(new Vector(xOverlap / 2, 0));
+            other.position = other.position.add(new Vector(-xOverlap / 2, 0));
+        } else {
+            this.position = this.position.add(new Vector(-xOverlap / 2, 0));
+            other.position = other.position.add(new Vector(xOverlap / 2, 0));
+        }
+
+        if (this.position.y > other.position.y) {
+            this.position = this.position.add(new Vector(0, yOverlap / 2));
+            other.position = other.position.add(new Vector(0, -yOverlap / 2));
+        } else {
+            this.position = this.position.add(new Vector(0, -yOverlap / 2));
+            other.position = other.position.add(new Vector(0, yOverlap / 2));
+        }
+    }
+
+    collide(other) {
+        //console.log("Colliding" + this + " and " + other);
+        this.colliding = true;
+        other.colliding = true;
+        this.correctOverlap(other);
+        let collision = other.position.sub(this.position);
+        let distance = collision.length();
+
+        collision = collision.div(distance);
+
+        let aci = this.velocity.dot(collision);
+        let bci = other.velocity.dot(collision);
+
+        this.velocity = this.velocity.add(collision.mult(bci - aci));
+        other.velocity = other.velocity.add(collision.mult(aci - bci));
+    }
+
+}
---
+++ game.js
@@ -0,0 +1,217 @@
+const BALL_SPAWN_WARMUP = 2;
+const BALL_SPAWN_INTERVAL = 20;
+const BALL_RADIUS = 20;
+const BALL_COUNT_INITIAL = 4;
+const BALL_SPEED_INITIAL = 3;
+
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+let intervalId;
+let balls = [];
+let player = new Ball(new Vector(0, 0), new Vector(0, 0), BALL_RADIUS);
+let state = "ended";
+let gameStartMillis;
+let lastBallSpawnMillis;
+let secondsSurvived;
+
+let run = function() {
+    update();
+    drawGame();
+}
+
+function start() {
+    for (let i = 1; i <= BALL_COUNT_INITIAL; i++) {
+        spawnBall();
+    }
+    player.active = true;
+    state = "playing";
+    gameStartMillis = Date.now() + (BALL_SPAWN_WARMUP * 1000);
+    intervalId = setInterval(run, 1000 / 120);
+}
+
+function startEnd() {
+    balls.push(player);
+    state = "ending";
+    secondsSurvived = Math.floor((Date.now() - gameStartMillis) / 1000);
+}
+
+function finishEnd() {
+    clearInterval(intervalId);
+    balls = [];
+    state = "ended";
+    checkHighscore(secondsSurvived);
+    drawMenu();
+    drawResults();
+    drawHighscore();
+}
+
+function update() {
+    for (const ball of balls) {
+        checkCollisions(ball);
+    }
+    for (const ball of balls) {
+        ball.colliding = false;
+        if (state === "ending") {
+            ball.velocity = ball.velocity.add(new Vector(0, 0.1));
+        }
+        if (ball.active) {
+            ball.move();
+        }
+    }
+    if (state === "playing" && (Date.now() - lastBallSpawnMillis) / 1000 >= BALL_SPAWN_INTERVAL) {
+        spawnBall();
+    } else if (state === "ending") {
+        let ballOnScreen = false;
+        for (const ball of balls) {
+            if (ball.position.y < canvas.height + 400) {
+                ballOnScreen = true;
+                break;
+            }
+        }
+        if (!ballOnScreen) {
+            finishEnd();
+        }
+    }
+
+}
+
+function checkCollisions(ball) {
+    if (state === "playing" && ball.hasCollided(player)) {
+        player.velocity = ball.velocity.mult(-1);
+        ball.collide(player);
+        startEnd();
+    }
+
+    if (ball.position.x <= ball.radius) {
+        ball.velocity = new Vector(Math.abs(ball.velocity.x), ball.velocity.y);
+        ball.position = new Vector(ball.radius, ball.position.y);
+    } else if (ball.position.x >= canvas.width - ball.radius) {
+        ball.velocity = new Vector(-Math.abs(ball.velocity.x), ball.velocity.y);
+        ball.position = new Vector(canvas.width - ball.radius, ball.position.y);
+    } else if (ball.position.y <= ball.radius) {
+        ball.velocity = new Vector(ball.velocity.x, Math.abs(ball.velocity.y))
+        ball.position = new Vector(ball.position.x, ball.radius);
+    } else if (state === "playing" && ball.position.y >= canvas.height - ball.radius) {
+        ball.velocity = new Vector(ball.velocity.x, -Math.abs(ball.velocity.y));
+        ball.position = new Vector(ball.position.x, canvas.height - ball.radius);
+    }
+
+    for (const collision of balls) {
+        if (ball !== collision && ball.hasCollided(collision) && !collision.colliding) {
+            ball.collide(collision);
+        }
+    }
+}
+
+function spawnBall() {
+    const ball = new Ball(new Vector(Math.floor(Math.random() * (canvas.width - 100)) + 50, 
+            Math.floor(Math.random() * (canvas.height - 100)) + 50), 
+            new Vector(0, 0), BALL_RADIUS);
+    balls.push(ball);
+    lastBallSpawnMillis = Date.now();
+    setTimeout(() => {
+        let hypDistance = ball.distance(player);
+        ball.velocity = new Vector(BALL_SPEED_INITIAL / hypDistance * (player.position.x - ball.position.x), 
+                BALL_SPEED_INITIAL / hypDistance * (player.position.y - ball.position.y));
+        ball.active = true;
+    }, BALL_SPAWN_WARMUP * 1000);
+}
+
+function drawGame() {
+    if (state === "ended") return;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 2;
+    for (const ball of balls) {
+        ctx.beginPath();
+        ctx.arc(ball.position.x, ball.position.y, ball.radius - 1, 0, Math.PI * 2, false);
+        if (ball.active) {
+            ctx.fillStyle = "red";
+            ctx.fill();
+            ctx.stroke();
+        } else {
+            ctx.fillStyle = "gray";
+            ctx.fill();
+        }
+    
+    }
+    ctx.fillStyle = "blue";
+    ctx.beginPath();
+    ctx.arc(player.position.x, player.position.y, player.radius - 1, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.stroke();
+}
+
+function drawMenu() {
+    ctx.textAlign = "center";
+    ctx.font = "bold 120px sans-serif";
+    ctx.fillStyle = "red";
+    ctx.fillText("Balls", canvas.width / 2, canvas.height / 2 - 25, canvas.width);
+    ctx.font = "40px sans-serif";
+    ctx.fillStyle = "blue";
+    ctx.fillText("Click to Play", canvas.width / 2, canvas.height / 2 + 50, canvas.width);
+}
+
+function drawResults() {
+    ctx.fillStyle = "black";
+    ctx.font = "bold 20px sans-serif";
+    ctx.fillText("Game over!", canvas.width / 2, 40);
+    ctx.font = "20px sans-serif";
+    ctx.fillText(`Time alive: ${secondsSurvived}s (${getBallCount(secondsSurvived)} balls)`, 
+            canvas.width / 2, 70);
+}
+
+function drawHighscore() {
+    ctx.fillStyle = "black";
+    ctx.font = "bold 20px sans-serif";
+    ctx.textDecoration = "underline";
+    ctx.fillText("Your Best:", canvas.width / 2, canvas.height - 60);
+    ctx.font = "20px sans-serif";
+    ctx.fillText(`${localStorage.getItem("hs")}s (${getBallCount(localStorage.getItem("hs"))} balls)`, 
+            canvas.width / 2, canvas.height - 30);
+}
+function checkHighscore(score) {
+    if (localStorage.getItem("hs") < secondsSurvived) {
+        localStorage.setItem("hs", secondsSurvived);
+        window.alert(`NEW HIGH SCORE!\n\nYou survived ${secondsSurvived} seconds and got to ${getBallCount(secondsSurvived)} balls!`);
+    }
+}
+
+function getBallCount(secondsSurvived) {
+    return Math.floor(secondsSurvived / BALL_SPAWN_INTERVAL) + BALL_COUNT_INITIAL;
+}
+
+function movePlayer(e) {
+    const rect = canvas.getBoundingClientRect();
+    let x = e.clientX - rect.x;
+    let y = e.clientY - rect.y;
+
+    if (x < player.radius) {
+        x = player.radius;
+    } else if (x > canvas.width - player.radius) {
+        x = canvas.width - player.radius;
+    }
+
+    if (y < player.radius) {
+        y = player.radius;
+    } else if (y > canvas.height - player.radius) {
+        y = canvas.height - player.radius;
+    }
+
+    player.position = new Vector(x, y);
+}
+
+document.body.addEventListener("mousemove", e => {
+    if (state === "playing") {
+        movePlayer(e);
+    }
+});
+document.getElementById("game").addEventListener("click", () => {
+    if (state === "ended") {
+        start();
+    }
+});
+drawMenu();
+if (localStorage.getItem("hs") !== null) {
+    drawHighscore();
+}
